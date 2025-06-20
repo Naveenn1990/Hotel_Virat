75462913533be515c0874f3861133084c2258d18
Parnets19 <parnetstech19@gmail.com>
2025-06-20 16:44:13 +0530

    done

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+// module.exports = {
+//   testEnvironment: 'node',
+//   testMatch: ['**/routes/**/*.test.js'],
+//   collectCoverage: true,
+//   coverageDirectory: 'coverage',
+//   verbose: true
+// };
